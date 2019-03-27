ea3d0d057ab070d0c1f8743f938d00a7d93d4f88
ds2713 <aspallds@aol.com>
2019-03-27 16:47:08 +0000

    Added a selection of new python files for a variety of tasks. Nothing useful yet, just learning the system.

---
+++ test_scripts/number_generator.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+length = 10
+T0 = np.random.rand(10)
+
+#for x in range (0, length):
+#	print(x)
+#	#print(np.random.rand(1))
+#	T0.append(np.random.rand(1)[0])
+	
+print(T0)
+plt.plot(T0)
+plt.show()
